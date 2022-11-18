b34b90e419de4d6fce167effd752363a76c99924
Nikita Lipin <nikitalipin97@gmail.com>
2022-11-18 16:17:19 +0300

    change config

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { AuthModule } from './auth/auth.module';
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: 'test',
+      database: process.env.DB_NAME,
       entities: [User],
       synchronize: process.env.NODE_ENV !== 'production',
     }),
