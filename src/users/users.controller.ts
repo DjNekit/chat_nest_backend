e925cca6c0f1f77891ea0356c9f9eefe5f03e8d3
Nikita Lipin <nikitalipin97@gmail.com>
2022-12-10 00:27:26 +0300

    rename query param

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param, Query, Req } from "@nestjs/common";
+import { UsersService } from "./users.service";
 
 @Controller({
   version: '1',
   path: '/users'
 })
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Get()
-  async getUsers() {
+  async getUsers(@Query('searchValue') searchValue) {
+    console.log(searchValue)
     return {
       users: []
     }
