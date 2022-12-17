ba0e1c583bd5836b65abb57e01bc64e8317a4ae6
Nikita Lipin <nikitalipin97@gmail.com>
2022-12-17 18:26:35 +0300

    mock chats

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
 import { AccessTokenGuard } from "../auth/guards/access-token.guard";
 import { User } from "../lib/decorators/user.decorator";
+import { ChatsService } from "./chats.service";
 
 @Controller({
   version: '1',
   path: '/chats'
 })
 export class ChatsController {
+  constructor(private chatsService: ChatsService) {}
+
   @Get()
   @UseGuards(AccessTokenGuard)
   async getChats(@User('id') userId: number) {
+    const chats = await this.chatsService.getChats(userId)
+
     return {
-      chats: []
+      chats
     }
   }
 }
--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -2,5 +2,32 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class ChatsService {
-
+  async getChats(userId: number) {
+    return [
+      {
+        id: 123456789,
+        creatorId: userId,
+        members: [
+          {
+            id: 1,
+            name: 'Nat'
+          },
+          {
+            id: 2,
+            name: 'Nat'
+          }
+        ],
+        isGroup: false,
+        lastMessage: 'Hello where',
+        messages: [
+          {
+            id: 13434225,
+            creatorId: 1,
+            text: 'Hello where',
+            createDate: Date.now()
+          }
+        ]
+      }
+    ]
+  }
 }
